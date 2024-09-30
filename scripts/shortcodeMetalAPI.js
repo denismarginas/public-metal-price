3e518f2297dc3588922e5b903a244e04f185f4e8
Denis Marginas <denismarginas09@gmail.com>
2024-09-30 15:33:36 +0300

    Disable ShortCode - CORS Acces Problem

--- scripts/shortcodeMetalAPI.js
+++ scripts/shortcodeMetalAPI.js
@@ -1,25 +1,25 @@
+/*
 document.addEventListener("DOMContentLoaded", function () {
-  // Function to fetch the metal price from the API
-  function fetchMetalPrice(metal, currency, element) {
+  function fetchMetalPrice(metal, currency) {
+    const proxyUrl = "";
     const apiUrl = `https://forex-data-feed.swissquote.com/public-quotes/bboquotes/instrument/${metal}/${currency}`;
 
-    fetch(apiUrl)
+    fetch(proxyUrl + apiUrl)
       .then((response) => response.json())
       .then((data) => {
         if (data && data[0] && data[0].spreadProfilePrices[0].bid) {
           const price = data[0].spreadProfilePrices[0].bid;
           const formattedPrice = price.toFixed(2);
-          element.textContent = formattedPrice; // Replace with the fetched price
+          console.log(`Price: ${formattedPrice}`);
         } else {
-          element.textContent = "Price not available.";
+          console.log("Price not available.");
         }
       })
       .catch(() => {
-        element.textContent = "Error fetching metal price.";
+        console.log("Error fetching metal price.");
       });
   }
 
-  // Function to replace shortcodes
   function replaceShortcodes() {
     const shortcodeRegex = /\[metal_price metal="(.*?)" currency="(.*?)"\]/g;
     const elements = document.querySelectorAll("body *:not(script):not(style)");
@@ -32,25 +32,22 @@ document.addEventListener("DOMContentLoaded", function () {
         const currency = match[2];
         const newElement = document.createElement("span");
 
-        // Fetch and replace shortcode with the price
         fetchMetalPrice(metal, currency, newElement);
         el.innerHTML = htmlContent.replace(match[0], newElement.outerHTML);
       }
     });
   }
 
-  // Initial replace on document load
   replaceShortcodes();
-
-  // Setup MutationObserver to detect new elements added to the DOM
+M
   const observer = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation) {
       if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
-        replaceShortcodes(); // Re-run replace on new DOM content
+        replaceShortcodes(); 
       }
     });
   });
 
-  // Start observing changes in the body
   observer.observe(document.body, { childList: true, subtree: true });
 });
+*/
